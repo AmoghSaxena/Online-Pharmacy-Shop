fd6bb96b741683f19e73411fa74626ce394fee71
AmoghSaxena <amoghsaxena1@gmail.com>
2021-01-25 03:04:50 +0530

    initial commit

---
+++ Quicken Pharmacy Project/Pharmacy/homeapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import ProductDetails, Newsletter, ContactUs, Login, Registeration
+admin.site.register(ProductDetails)
+admin.site.register(Newsletter)
+admin.site.register(ContactUs)
+admin.site.register(Login)
+admin.site.register(Registeration)
+# Register your models here.
